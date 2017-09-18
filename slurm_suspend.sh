26511738eeac165a62cd7649d397f9bbdc630c2e
Eric Coulter <jecoulte@iu.edu>
2017-09-18 16:12:54 +0000

    Added remove from ansible hosts

--- slurm_suspend.sh
+++ slurm_suspend.sh
@@ -8,5 +8,6 @@ echo "Node suspend invoked: $0 $*" >> $log_loc
 
 for host in $(scontrol show hostname $1)
 do
-openstack server delete $host
+  sed -i "s/$host//" /etc/ansible/hosts
+  openstack server delete $host
 done
