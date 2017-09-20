6255a74d8214a84b6828bf6756d095b06ccaccc5
Eric Coulter <jecoulte@iu.edu>
2017-09-20 17:53:16 +0000

    improved ansible hosts cleanup

--- slurm_suspend.sh
+++ slurm_suspend.sh
@@ -8,6 +8,6 @@ echo "Node suspend invoked: $0 $*" >> $log_loc
 
 for host in $(scontrol show hostname $1)
 do
-  sed -i "s/$host//" /etc/ansible/hosts
+  sed -i "s/^\s+$host\s+*//" /etc/ansible/hosts
   openstack server delete $host
 done
