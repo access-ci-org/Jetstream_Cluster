830599918ccb32eafad97a3ba0578f3f93d756e4
jecoulte <jecoulte@iu.edu>
2017-09-27 15:39:09 +0000

    changed to stop instead of delete

--- slurm_suspend.sh
+++ slurm_suspend.sh
@@ -9,5 +9,5 @@ echo "Node suspend invoked: $0 $*" >> $log_loc
 for host in $(scontrol show hostname $1)
 do
   sed -i "s/^\s+$host\s+*//" /etc/ansible/hosts
-  openstack server delete $host
+  openstack server stop $host
 done
