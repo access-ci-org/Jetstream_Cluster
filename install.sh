a8d631323103c2c8cf37d8e3b31a96374c989316
Eric Coulter <jecoulte@iu.edu>
2017-10-10 16:07:32 +0000

    added mailx for sending slurm info emails

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ fi
 
 yum -y install https://github.com/openhpc/ohpc/releases/download/v1.3.GA/ohpc-release-1.3-1.el7.x86_64.rpm
 
-yum -y install ohpc-slurm-server ansible
+yum -y install ohpc-slurm-server ansible mailx
 
 pip install python-openstackclient
 
