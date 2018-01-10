cc28cf84c91c091601c3b1ba198173df49deccc6
Eric Coulter <jecoulte@iu.edu>
2018-01-10 15:19:57 -0500

    added node state check to suspend

--- slurm_suspend.sh
+++ slurm_suspend.sh
@@ -3,11 +3,19 @@
 source /etc/slurm/openrc.sh
 
 log_loc=/var/log/slurm_elastic.log
+#log_loc=/dev/stdout 
 
 echo "Node suspend invoked: $0 $*" >> $log_loc
+
+hostlist=$(openstack server list)
 
 for host in $(scontrol show hostname $1)
 do
   sed -i "s/^$host.*//" /etc/ansible/hosts
-  openstack server stop $host
+  if [[ "$(echo "$hostlist" | awk -v host=$host '$0 ~ host {print $6}')" == "ACTIVE" ]]; then 
+    echo "Stopping $host" >> $log_loc
+    openstack server stop $host
+  else
+    echo "$host not ACTIVE" >> $log_loc
+  fi
 done
