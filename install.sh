780bb32c887a4695240260e9963f5fdce0843c1e
Eric Coulter <jecoulte@iu.edu>
2018-08-29 12:18:27 -0400

    gave slurm user write privs on /etc/hosts

--- install.sh
+++ install.sh
@@ -97,6 +97,8 @@ cp /etc/munge/munge.key /etc/slurm/.munge.key
 
 chown slurm:slurm /etc/slurm/.munge.key
 
+setfacl -m u:slurm:rw /etc/hosts
+
 #How to generate a working openrc in the cloud-init script for this? Bash vars available?
 # Gonna be tough, since openrc requires a password...
 cp openrc.sh /etc/slurm/
