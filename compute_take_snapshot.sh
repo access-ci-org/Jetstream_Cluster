72b2da61eb8617b54fb7f0efa6f5e1888f787648
Eric Coulter <jecoulte@iu.edu>
2019-01-07 21:44:18 +0000

    Script to take OS snapshot
    written in bash due to deficiencies in the ansible os_* modules.
    Stops the instance, waits, and takes the snapshot. Does not
    remove the instance.
    
    Signed-off-by: Eric Coulter <jecoulte@iu.edu>

---
+++ compute_take_snapshot.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+source openrc.sh
+
+compute_image="${OS_USERNAME}-compute-image-$(date +%m-%d-%Y)"
+compute_instance="compute-${OS_USERNAME}-base-instance"
+
+openstack server stop $compute_instance
+
+count=0
+declare -i count
+until [[ $count -ge 12 || "$shutoff_check" =~ "SHUTOFF" ]];
+do
+  shutoff_check=$(openstack server show -f value -c status $compute_instance)
+  count+=1
+  sleep 5
+done
+
+openstack server image create --name $compute_image $compute_instance
+
+count=0
+declare -i count
+until [[ $count -ge 12 || "$instance_check" =~ "active" ]];
+do
+  instance_check=$(openstack image show -f value -c status $compute_image)
+  count+=1
+  sleep 10
+done
+
+echo "Done after $count sleeps."
+openstack image show $compute_image
