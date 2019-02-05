f37d1770dcc85d9b92a4aacfdb2fb7b1ecad080d
Eric Coulter <jecoulte@iu.edu>
2019-02-05 20:36:56 +0000

    Edits for safety and clarity
    Added non-zero exit code if image not available after 300s
    Added auto-delete if image re-built within same day. Still need
    to fix issue of image sorting in slurm_resume.
    
    Signed-off-by: Eric Coulter <jecoulte@iu.edu>

--- compute_take_snapshot.sh
+++ compute_take_snapshot.sh
@@ -16,6 +16,12 @@ do
   sleep 5
 done
 
+image_check=$(openstack image show -f value -c name $compute_image)
+if [[ -n $image_check ]];
+then
+  openstack image delete $compute_image
+fi
+
 openstack server image create --name $compute_image $compute_instance
 
 count=0
@@ -26,6 +32,12 @@ do
   count+=1
   sleep 15
 done
+
+if [[ $count -ge 20 ]];
+then
+  echo "Image still in queued status after 300 seconds"
+  exit 2
+fi
 
 echo "Done after $count sleeps."
 openstack image show $compute_image
