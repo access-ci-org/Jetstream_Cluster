0d388cce5134a225963798ce76042963b5e8a938
Eric Coulter <jecoulte@iu.edu>
2019-02-11 21:14:53 +0000

    Added bind-utils and clean-os.sh script
    Signed-off-by: Eric Coulter <jecoulte@iu.edu>

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ fi
 
 yum -y install https://github.com/openhpc/ohpc/releases/download/v1.3.GA/ohpc-release-1.3-1.el7.x86_64.rpm
 
-yum -y install ohpc-slurm-server vim ansible mailx lmod-ohpc bash-completion gnu-compilers-ohpc openmpi-gnu-ohpc lmod-defaults-gnu-openmpi-ohpc moreutils
+yum -y install ohpc-slurm-server vim ansible mailx lmod-ohpc bash-completion gnu-compilers-ohpc openmpi-gnu-ohpc lmod-defaults-gnu-openmpi-ohpc moreutils bind-utils
 
 
 pip install python-openstackclient
@@ -120,6 +120,7 @@ chown slurm:slurm /etc/slurm/prevent-updates.ci
 mkdir /var/log/slurm
 
 touch /var/log/slurm/slurm_elastic.log
+touch /var/log/slurm/os_clean.log
 
 chown -R slurm:slurm /var/log/slurm
 
