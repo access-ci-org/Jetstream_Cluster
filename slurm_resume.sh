85741e5783c1e6097d8a4ef11322ee253a3fc309
Eric Coulter <jecoulte@iu.edu>
2019-03-12 17:05:53 -0400

    Shortened slurm_resume
    Commented out unnecessary ip-mangling bits at the end of slurm_resume,
    in addition to extra ansible tasks. Greatly improves the speed of
    the script, and now finishes *before* a job is actually started
    on the node. Previously all this stuff would run after the node
    was actually responding to slurmctld, causing all sorts of issues.
    
    Signed-off-by: Eric Coulter <jecoulte@iu.edu>

--- slurm_resume.sh
+++ slurm_resume.sh
@@ -49,55 +49,56 @@ do
   new_ip=$(openstack server show $host | awk '/addresses/ {print gensub(/^.*=/,"","g",$4)}')
   echo "$host ip is $new_ip" >> $log_loc 
 
-  if [[ $new_ip =~ "|" ]]; then
-    new_ip=$(host $host | sed 's/.*address //')
-    echo "Falling back on dns hostname for $host: $new_ip" >> $log_loc
-  fi
-
-  # now that we have the ip, make sure it's in etc/hosts, as we can't always trust to dns
-  ip_check=$(grep $new_ip /etc/hosts)
-#  echo "Found $ip_check in /etc/hosts new_ip check" >> $log_loc
-  host_check=$(grep $host /etc/hosts)
-#  echo "Found $host_check in /etc/hosts host_check" >> $log_loc
-  if [[ -n $ip_check && ! ( $ip_check =~ $host ) ]]; then
-   # this is bad, as slurm_suspend should remove the /etc/hosts entry for old nodes
-   # ACTUALLY, this causes an issue with a compute node that receives the same IP and hasn't been removed from /etc/hosts...
-   echo "OVERLAPPING ENTRY FOR $new_ip of $host in /etc/hosts: $ip_check" >> $log_loc
-   exit 2
-  fi
-  if [[ -n $host_check && ! ( $host_check =~ $new_ip ) ]]; then
-    echo "REPLACING $host_check with $new_ip for $host" >> $log_loc
-    sed "s/.*$host/$new_ip $host/" /etc/hosts 2>&1 | sponge /etc/hosts >> $log_loc # due to the sticky bit required on /etc by munge
-#    echo "$? result of sed" >> $log_loc
-  fi
-  if [[ -z $host_check ]]; then 
-    echo "ADDING NEW ENTRY for $host at $new_ip IN /etc/hosts" >> $log_loc
-    echo "$new_ip $host" >> /etc/hosts
-  fi
-
-  sleep 10 # to give sshd time to be available
-
-  test_hostname=$(ssh -q -F /etc/ansible/ssh.cfg $host 'hostname' | tee -a $log_loc)
-  #  echo "test1: $test_hostname"
-  until [[ -n "${test_hostname}" ]]; do
-    sleep 5
-    test_hostname=$(ssh -q -F /etc/ansible/ssh.cfg $host 'hostname' | tee -a $log_loc)
-#    echo "TESTING SSH ACCESS: $test_hostname" >> $log_loc
-  done
-
-  #reset the hostname JIC - no need for this w/ correctly built image
-  #hostname_set_result=$(ansible -m hostname -a "name=$host" $host)
-  #add users in case any added since image build
-  user_add_result=$(ansible -m script -a "/tmp/add_users.sh" $host)
-#  echo "Tried to add users: " $user_add_result >> $log_loc
-  hosts_add_result=$(ansible -m copy -a "src=/etc/hosts dest=/etc/hosts" $host)
-#  echo "Tried to add hosts $hosts_add_result" >> $log_loc
-  slurm_sync_result=$(ansible -m copy -a "src=/etc/slurm/slurm.conf dest=/etc/slurm/slurm.conf" $host)
-#  echo "Tried to sync slurm.conf $slurm_sync_result" >> $log_loc
-  slurmd_start_result=$(ansible -m service -a "name=slurmd state=started enabled=yes" $host)
-#  echo "Tried to start slurmd $slurm_start_result" >> $log_loc
-
-#Now, safe to update slurm w/ node info
+#This section proven redundant and possibly harmful on the IU cloud - not tested on TACC yet!
+#  if [[ $new_ip =~ "|" ]]; then
+#    new_ip=$(host $host | sed 's/.*address //')
+#    echo "Falling back on dns hostname for $host: $new_ip" >> $log_loc
+#  fi
+#
+#  # now that we have the ip, make sure it's in etc/hosts, as we can't always trust to dns
+#  ip_check=$(grep $new_ip /etc/hosts)
+##  echo "Found $ip_check in /etc/hosts new_ip check" >> $log_loc
+#  host_check=$(grep $host /etc/hosts)
+##  echo "Found $host_check in /etc/hosts host_check" >> $log_loc
+#  if [[ -n $ip_check && ! ( $ip_check =~ $host ) ]]; then
+#   # this is bad, as slurm_suspend should remove the /etc/hosts entry for old nodes
+#   # ACTUALLY, this causes an issue with a compute node that receives the same IP and hasn't been removed from /etc/hosts...
+#   echo "OVERLAPPING ENTRY FOR $new_ip of $host in /etc/hosts: $ip_check" >> $log_loc
+#   exit 2
+#  fi
+#  if [[ -n $host_check && ! ( $host_check =~ $new_ip ) ]]; then
+#    echo "REPLACING $host_check with $new_ip for $host" >> $log_loc
+#    sed "s/.*$host/$new_ip $host/" /etc/hosts 2>&1 | sponge /etc/hosts >> $log_loc # due to the sticky bit required on /etc by munge
+##    echo "$? result of sed" >> $log_loc
+#  fi
+#  if [[ -z $host_check ]]; then 
+#    echo "ADDING NEW ENTRY for $host at $new_ip IN /etc/hosts" >> $log_loc
+#    echo "$new_ip $host" >> /etc/hosts
+#  fi
+#
+#  sleep 10 # to give sshd time to be available
+#
+#  test_hostname=$(ssh -q -F /etc/ansible/ssh.cfg $host 'hostname' | tee -a $log_loc)
+#  #  echo "test1: $test_hostname"
+#  until [[ -n "${test_hostname}" ]]; do
+#    sleep 5
+#    test_hostname=$(ssh -q -F /etc/ansible/ssh.cfg $host 'hostname' | tee -a $log_loc)
+##    echo "TESTING SSH ACCESS: $test_hostname" >> $log_loc
+#  done
+#
+#  #reset the hostname JIC - no need for this w/ correctly built image
+#  #hostname_set_result=$(ansible -m hostname -a "name=$host" $host)
+#  #add users in case any added since image build
+#  user_add_result=$(ansible -m script -a "/tmp/add_users.sh" $host)
+##  echo "Tried to add users: " $user_add_result >> $log_loc
+#  hosts_add_result=$(ansible -m copy -a "src=/etc/hosts dest=/etc/hosts" $host)
+##  echo "Tried to add hosts $hosts_add_result" >> $log_loc
+#  slurm_sync_result=$(ansible -m copy -a "src=/etc/slurm/slurm.conf dest=/etc/slurm/slurm.conf" $host)
+##  echo "Tried to sync slurm.conf $slurm_sync_result" >> $log_loc
+#  slurmd_start_result=$(ansible -m service -a "name=slurmd state=started enabled=yes" $host)
+##  echo "Tried to start slurmd $slurm_start_result" >> $log_loc
+#
+##Now, safe to update slurm w/ node info
   scontrol update nodename=$host nodeaddr=$new_ip >> $log_loc
 
 done
