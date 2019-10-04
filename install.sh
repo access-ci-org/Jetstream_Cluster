2b1ee165162fb5eb8948d5c5ec59cc71271889f1
Eric Coulter <jecoulte@iu.edu>
2019-10-04 14:49:42 -0400

    added active flag and tail to image_name
    Signed-off-by: Eric Coulter <jecoulte@iu.edu>

--- install.sh
+++ install.sh
@@ -196,8 +196,7 @@ echo -e "/opt/ohpc/pub 10.0.0.0/24(rw,no_root_squash)" >> /etc/exports
 
 #Get latest CentOS7 minimal image for base - if os_image_facts or the os API allowed for wildcards,
 #  this would be different. But this is the world we live in.
-#centos_base_image=$(openstack image list | grep -i js-api-featured-centos7 | grep -vi intel | awk '{print $4}')
-centos_base_image=$(openstack image list | grep -iE "API-Featured-centos7-[[:alpha:]]{3,4}-[0-9]{2}-[0-9]{4}" | awk '{print $4}')
+centos_base_image=$(openstack image list --active | grep -iE "API-Featured-centos7-[[:alpha:]]{3,4}-[0-9]{2}-[0-9]{4}" | awk '{print $4}' | tail -n 1)
 sed -i "s/\(\s*compute_base_image: \).*/\1\"${centos_base_image}\"/" compute_build_base_img.yml | head -n 10
 
 #create temporary script to add local users
