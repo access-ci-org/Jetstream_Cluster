8d0cee053722730b91a7eb1c114ebdc9ebb37942
Eric Coulter <jecoulte@iu.edu>
2020-03-02 14:58:20 -0500

    update slurm.conf at node creation time
    pull in slurm.conf at node creation via
    cloud-config write_files module, rather than
    having to rebuild the instance image when only changing
    number/size of nodes or partitions
    
    Signed-off-by: Eric Coulter <jecoulte@iu.edu>

--- slurm_resume.sh
+++ slurm_resume.sh
@@ -15,12 +15,15 @@ for host in $(scontrol show hostname $1)
 do
 
 #Launch compute nodes and check for new ip address in same subprocess - with 2s delay between Openstack requests
+    #--user-data <(cat /etc/slurm/prevent-updates.ci && echo -e "hostname: $host \npreserve_hostname: true\ndebug:") \
+    # the current --user-data pulls in the slurm.conf as well, to avoid rebuilding node images
+		# when adding / changing partitions
     (echo "creating $host" >> $log_loc;
     openstack server create $host \
     --flavor $node_size \
     --image $node_image \
     --key-name $key_name \
-    --user-data <(cat /etc/slurm/prevent-updates.ci && echo -e "hostname: $host \npreserve_hostname: true\ndebug:") \
+		--user-data <(cat /etc/slurm/prevent-updates.ci && echo -e "hostname: $host \npreserve_hostname: true\ndebug:" && echo -e "write_files:\n  - encoding: b64\n    owner: slurm\n    path: /etc/slurm/slurm.conf\n    permissions: 0644\n    content: |\n$(cat /etc/slurm/slurm.conf | base64 | sed 's/^/      /')\n") \
     --security-group ${OS_USERNAME}-global-ssh --security-group ${OS_USERNAME}-cluster-internal \
     --nic net-id=$network_name 2>&1 \
     | tee -a $log_loc | awk '/status/ {print $4}' >> $log_loc 2>&1;
