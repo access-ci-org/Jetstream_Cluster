5561d8bd29877e2719b17821448649aa8759c1ef
Eric Coulter <jecoulte@iu.edu>
2020-03-16 15:59:07 -0400

    Changes to file sync with compute nodes
    Syncing files via user-data now, enables greater flexibility.
    Add f'n to generate write_files blocks, for adding future files.
    Currently syncs slurm.conf, passwd, and etc/hosts
    /etc/hosts is needed for cases where cloud dns doesn't work.
    
    Signed-off-by: Eric Coulter <jecoulte@iu.edu>

--- slurm_resume.sh
+++ slurm_resume.sh
@@ -8,6 +8,22 @@ key_name="${OS_USERNAME}-${OS_PROJECT_NAME}-slurm-key"
 network_name=${OS_USERNAME}-elastic-net
 log_loc=/var/log/slurm/slurm_elastic.log
 
+#def f'n to generate a write_files entry for cloud-config for copying over a file
+# arguments are owner path permissions file_to_be_copied
+# All calls to this must come after an "echo "write_files:\n"
+generate_write_files () {
+#This is generating YAML, so... spaces are important.
+echo -e "  - encoding: b64\n    owner: $1\n    path: $2\n    permissions: $3\n    content: |\n$(cat $4 | base64 | sed 's/^/      /')"
+}
+
+user_data_long="$(cat /etc/slurm/prevent-updates.ci)\n"
+user_data_long+="$(echo -e "hostname: $host \npreserve_hostname: true\ndebug: \nmanage_etc_hosts: false\n")\n"
+user_data_long+="$(echo -e "write_files:")\n"
+user_data_long+="$(generate_write_files slurm "/etc/slurm/slurm.conf" "0644" "/etc/slurm/slurm.conf")\n"
+user_data_long+="$(generate_write_files root "/etc/hosts" "0664" "/etc/hosts")\n"
+user_data_long+="$(generate_write_files root "/etc/passwd" "0644" "/etc/passwd")\n"
+#Done generating the cloud-config for compute nodes
+
 echo "Node resume invoked: $0 $*" >> $log_loc
 
 #First, loop over hosts and run the openstack create commands for *all* resume hosts at once.
@@ -16,14 +32,15 @@ do
 
 #Launch compute nodes and check for new ip address in same subprocess - with 2s delay between Openstack requests
     #--user-data <(cat /etc/slurm/prevent-updates.ci && echo -e "hostname: $host \npreserve_hostname: true\ndebug:") \
-    # the current --user-data pulls in the slurm.conf as well, to avoid rebuilding node images
-		# when adding / changing partitions
+    # the current --user-data pulls in the slurm.conf and /etc/passwd as well, to avoid rebuilding node images
+    # when adding / changing partitions
+
     (echo "creating $host" >> $log_loc;
     openstack server create $host \
     --flavor $node_size \
     --image $node_image \
     --key-name $key_name \
-		--user-data <(cat /etc/slurm/prevent-updates.ci && echo -e "hostname: $host \npreserve_hostname: true\ndebug:" && echo -e "write_files:\n  - encoding: b64\n    owner: slurm\n    path: /etc/slurm/slurm.conf\n    permissions: 0644\n    content: |\n$(cat /etc/slurm/slurm.conf | base64 | sed 's/^/      /')\n") \
+    --user-data <(echo -e "${user_data_long}") \
     --security-group ${OS_USERNAME}-global-ssh --security-group ${OS_USERNAME}-cluster-internal \
     --nic net-id=$network_name 2>&1 \
     | tee -a $log_loc | awk '/status/ {print $4}' >> $log_loc 2>&1;
