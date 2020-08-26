633e08a0e4072ffb0c2c6287876ba7d4dafbcd6a
Eric Coulter <jecoulte@iu.edu>
2020-08-26 14:29:43 -0400

    Updated naming variables
    Added OS_PREFIX and separate OS_?_NAME variables for
    cluster components to allow easier handling of multiple
    VC's on the same allocation, and generally easier
    name handling.
    
    Signed-off-by: Eric Coulter <jecoulte@iu.edu>

--- headnode_create.sh
+++ headnode_create.sh
@@ -48,29 +48,37 @@ quota_check "routers" "router" 1
 quota_check "key-pairs" "keypair" 1
 quota_check "instances" "server" 1
 
+#To more easily avoid collisions with multiple VC's on a single alloc
+OS_PREFIX=${OS_USERNAME}
+OS_NETWORK_NAME=${OS_PREFIX}-elastic-net
+OS_SUBNET_NAME=${OS_PREFIX}-elastic-subnet
+OS_ROUTER_NAME=${OS_PREFIX}-elastic-router
+OS_SSH_SECGROUP_NAME=${OS_PREFIX}-ssh-global
+OS_INTERNAL_SECGROUP_NAME=${OS_PREFIX}-internal
+OS_KEYPAIR_NAME=${OS_USERNAME}-elastic-key
+
 # Ensure that the correct private network/router/subnet exists
 if [[ -z "$(openstack network list | grep ${OS_USERNAME}-elastic-net)" ]]; then
-  openstack network create ${OS_USERNAME}-elastic-net
-  openstack subnet create --network ${OS_USERNAME}-elastic-net --subnet-range 10.0.0.0/24 ${OS_USERNAME}-elastic-subnet1
+  openstack network create ${OS_NETWORK_NAME}
+  openstack subnet create --network ${OS_NETWORK_NAME} --subnet-range 10.0.0.0/24 ${OS_SUBNET_NAME}
 fi
 ##openstack subnet list
 if [[ -z "$(openstack router list | grep ${OS_USERNAME}-elastic-router)" ]]; then
-  openstack router create ${OS_USERNAME}-elastic-router
-  openstack router add subnet ${OS_USERNAME}-elastic-router ${OS_USERNAME}-elastic-subnet1
-  openstack router set --external-gateway public ${OS_USERNAME}-elastic-router
+  openstack router create ${OS_ROUTER_NAME}
+  openstack router add subnet ${OS_ROUTER_NAME} ${OS_SUBNET_NAME}
+  openstack router set --external-gateway public ${OS_ROUTER_NAME}
 fi
-#openstack router show ${OS_USERNAME}-api-router
 
 security_groups=$(openstack security group list -f value)
-if [[ ! ("${security_groups}" =~ "${OS_USERNAME}-global-ssh") ]]; then
-  openstack security group create --description "ssh \& icmp enabled" ${OS_USERNAME}-global-ssh
-  openstack security group rule create --protocol tcp --dst-port 22:22 --remote-ip 0.0.0.0/0 ${OS_USERNAME}-global-ssh
-  openstack security group rule create --protocol icmp ${OS_USERNAME}-global-ssh
+if [[ ! ("${security_groups}" =~ "${OS_SSH_SECGROUP_NAME}") ]]; then
+  openstack security group create --description "ssh \& icmp enabled" ${OS_SSH_SECGROUP_NAME}
+  openstack security group rule create --protocol tcp --dst-port 22:22 --remote-ip 0.0.0.0/0 ${OS_SSH_SECGROUP_NAME}
+  openstack security group rule create --protocol icmp ${OS_SSH_SECGROUP_NAME}
 fi
-if [[ ! ("${security_groups}" =~ "${OS_USERNAME}-cluster-internal") ]]; then
-  openstack security group create --description "internal group for cluster" ${OS_USERNAME}-cluster-internal
-  openstack security group rule create --protocol tcp --dst-port 1:65535 --remote-ip 10.0.0.0/0 ${OS_USERNAME}-cluster-internal
-  openstack security group rule create --protocol icmp ${OS_USERNAME}-cluster-internal
+if [[ ! ("${security_groups}" =~ "${OS_INTERNAL_SECGROUP_NAME}") ]]; then
+  openstack security group create --description "internal group for cluster" ${OS_INTERNAL_SECGROUP_NAME}
+  openstack security group rule create --protocol tcp --dst-port 1:65535 --remote-ip 10.0.0.0/0 ${OS_INTERNAL_SECGROUP_NAME}
+  openstack security group rule create --protocol icmp ${OS_INTERNAL_SECGROUP_NAME}
 fi
 
 #Check if ${HOME}/.ssh/id_rsa.pub exists in JS
@@ -81,17 +89,16 @@ openstack_keys=$(openstack keypair list -f value)
 
 home_key_in_OS=$(echo "${openstack_keys}" | awk -v mykey="${home_key_fingerprint}" '$2 ~ mykey {print $1}')
 
-if [[ -n "${home_key_in_OS}" ]]; then
-  OS_keyname=${home_key_in_OS}
-elif [[ -n $(echo "${openstack_keys}" | grep ${OS_USERNAME}-elastic-key) ]]; then
-  openstack keypair delete ${OS_USERNAME}-elastic-key
+if [[ -n "${home_key_in_OS}" ]]; then 
+	#RESET this to key that's already in OS
+  OS_KEYPAIR_NAME=${home_key_in_OS}
+elif [[ -n $(echo "${openstack_keys}" | grep ${OS_KEYPAIR_NAME}) ]]; then
+  openstack keypair delete ${OS_KEYPAIR_NAME}
 # This doesn't need to depend on the OS_PROJECT_NAME, as the slurm-key does, in install.sh and slurm_resume
-  openstack keypair create --public-key ${HOME}/.ssh/id_rsa.pub ${OS_USERNAME}-elastic-key
-  OS_keyname=${OS_USERNAME}-elastic-key
+  openstack keypair create --public-key ${HOME}/.ssh/id_rsa.pub ${OS_KEYPAIR_NAME}
 else
 # This doesn't need to depend on the OS_PROJECT_NAME, as the slurm-key does, in install.sh and slurm_resume
-  openstack keypair create --public-key ${HOME}/.ssh/id_rsa.pub ${OS_USERNAME}-elastic-key
-  OS_keyname=${OS_USERNAME}-elastic-key
+  openstack keypair create --public-key ${HOME}/.ssh/id_rsa.pub ${OS_KEYPAIR_NAME}
 fi
 
 #centos_base_image=$(openstack image list --status active | grep -iE "API-Featured-centos7-[[:alpha:]]{3,4}-[0-9]{2}-[0-9]{4}" | awk '{print $4}' | tail -n 1)
@@ -101,20 +108,20 @@ echo -e "openstack server create\
         --user-data prevent-updates.ci \
         --flavor m1.small \
         --image ${centos_base_image} \
-        --key-name ${OS_keyname} \
-        --security-group ${OS_USERNAME}-global-ssh \
-        --security-group ${OS_USERNAME}-cluster-internal \
-        --nic net-id=${OS_USERNAME}-elastic-net \
+        --key-name ${OS_KEYPAIR_NAME} \
+        --security-group ${OS_SSH_SECGROUP_NAME} \
+        --security-group ${OS_INTERNAL_SECGROUP_NAME} \
+        --nic net-id=${OS_NETWORK_NAME} \
         ${server_name}"
 
 openstack server create \
         --user-data prevent-updates.ci \
         --flavor m1.small \
         --image ${centos_base_image} \
-        --key-name ${OS_keyname} \
-        --security-group ${OS_USERNAME}-global-ssh \
-        --security-group ${OS_USERNAME}-cluster-internal \
-        --nic net-id=${OS_USERNAME}-elastic-net \
+        --key-name ${OS_KEYPAIR_NAME} \
+        --security-group ${OS_SSH_SECGROUP_NAME} \
+        --security-group ${OS_INTERNAL_SECGROUP_NAME} \
+        --nic net-id=${OS_NETWORK_NAME} \
         ${server_name}
 
 public_ip=$(openstack floating ip create public | awk '/floating_ip_address/ {print $4}')
